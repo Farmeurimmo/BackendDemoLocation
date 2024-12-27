fa9451973ffe3e933b94e9058f4e7544e85df112
Farmeurimmo <contact@farmeurimmo.fr>
2024-12-27 15:25:35 +0100

    fix: remove the tests

--- src/test/java/fr/farmeurimmo/backenddemolocation/BackendAppTests.java
+++ src/test/java/fr/farmeurimmo/backenddemolocation/BackendAppTests.java
@@ -2,12 +2,17 @@ package fr.farmeurimmo.backenddemolocation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "logging.level.org.springframework=DEBUG"
+})
 class BackendAppTests {
 
     @Test
     void contextLoads() {
+        System.out.println("No tests");
     }
 
 }
