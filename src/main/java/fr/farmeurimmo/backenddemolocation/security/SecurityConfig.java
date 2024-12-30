70c6fc37dabaa2bb2c643821df1efa667b30e8df
Farmeurimmo <contact@farmeurimmo.fr>
2024-12-30 16:11:49 +0100

    fix: CORS

--- src/main/java/fr/farmeurimmo/backenddemolocation/security/SecurityConfig.java
+++ src/main/java/fr/farmeurimmo/backenddemolocation/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://*.farmeurimmo.fr")
+                .allowedOrigins("https://frontend-demo-location.farmeurimmo.fr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
